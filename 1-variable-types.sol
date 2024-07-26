19f322036d6c27e82fed6d6761cc60770314ccbf
zntb <zenetibi@yahoo.com>
2024-07-26 18:44:51 +0300

    1-Basic variable types

---
+++ 1-variable-types.sol
@@ -0,0 +1,13 @@
+// Basic variable types
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+    bool HasFavoriteNumber = true;
+    string favoriteNumberIntext = "eighty-eight";
+    int256 favoriteInt = -88;
+    address myAddress = 0x28556Cc7CBa0A5AF1170fB9eD4E634a811366E70;
+    bytes32 favoriteBytes32 = "cat"; // 0xhffdkfh
+    uint256 favoriteNumber = 5;
+}

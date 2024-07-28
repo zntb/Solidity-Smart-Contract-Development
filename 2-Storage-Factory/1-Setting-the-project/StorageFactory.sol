6b3517f89d82fb4457785faa17e1d9da944a253f
zntb <zenetibi@yahoo.com>
2024-07-28 16:04:07 +0300

    12-Solidity imports

--- 2-Storage-Factory/1-Setting-the-project/StorageFactory.sol
+++ 2-Storage-Factory/1-Setting-the-project/StorageFactory.sol
@@ -1,31 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract SimpleStorage {
-    uint256 myFavoriteNumber;
-
-    struct Person {
-        uint256 favoriteNumber;
-        string name;
-    }
-    // uint256[] public anArray;
-    Person[] public listOfPeople;
-
-    mapping(string => uint256) public nameToFavoriteNumber;
-
-    function store(uint256 _favoriteNumber) public virtual {
-        myFavoriteNumber = _favoriteNumber;
-    }
-
-    function retrieve() public view returns (uint256) {
-        return myFavoriteNumber;
-    }
-
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push(Person(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
-}
+// import "./SimpleStorage.sol"; or:
+import {SimpleStorage, SimpleStorage2, SimpleStorage3, SimpleStorage4} from "./SimpleStorage.sol";
 
 contract StorageFactory {
     SimpleStorage public simpleStorage;

d73ebbcdc2117093850830b2ecee7c58e512c80d
zntb <zntbdev@proton.me>
2024-07-30 16:26:37 +0300

    19-Sending ETH through a function

--- 3-Fund-Me/fundMe.sol
+++ 3-Fund-Me/fundMe.sol
@@ -6,7 +6,12 @@
 pragma solidity ^0.8.19;
 
 contract FundMe {
-    function fund() public {}
+    function fund() public payable {
+        // allow users to send $
+        // have a minimum of $ sent
+        // 1. How do we send ETH to this contract?
+        require(msg.value > 1e18, "Did'nt send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
+    }
 
-    function withdraw() public {}
+    // function withdraw() public {}
 }

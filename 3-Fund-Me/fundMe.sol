a76961d03a5786624a40ed71cb1d8fd14f2a3e04
zntb <zntbdev@proton.me>
2024-08-01 17:37:03 +0300

    35-Smart contract constructor

--- 3-Fund-Me/fundMe.sol
+++ 3-Fund-Me/fundMe.sol
@@ -16,6 +16,12 @@ contract FundMe {
     mapping(address funder => uint256 amountFunded)
         public addressToAmountFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(
             msg.value.getConversionRate() >= minimumUsd,
@@ -28,7 +34,7 @@ contract FundMe {
     }
 
     function withdraw() public {
-        // for loop
+        require(msg.sender == owner, "Only the owner can withdraw funds");
         for (
             uint256 founderIndex = 0;
             founderIndex < funders.length;
@@ -42,13 +48,6 @@ contract FundMe {
         funders = new address[](0);
         // Withdraw the funds
 
-        // 1- Transfer:
-        // msg.sender = address
-        // payable(msg.sender) = payable address
-        payable(msg.sender).transfer(address(this).balance);
-        // 2 Send:
-        bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        require(sendSuccess, "Send failed");
         // 3 Call - recomended way
         (bool callSuccess, ) = payable(msg.sender).call{
             value: address(this).balance

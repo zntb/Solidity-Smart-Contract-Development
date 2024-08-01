340eaaa9efeb7e439cda7d4a780591afac25f62c
zntb <zntbdev@proton.me>
2024-08-01 17:28:29 +0300

    34-Sending ETH from a contract

--- 3-Fund-Me/fundMe.sol
+++ 3-Fund-Me/fundMe.sol
@@ -41,5 +41,19 @@ contract FundMe {
         // Reset the array
         funders = new address[](0);
         // Withdraw the funds
+
+        // 1- Transfer:
+        // msg.sender = address
+        // payable(msg.sender) = payable address
+        payable(msg.sender).transfer(address(this).balance);
+        // 2 Send:
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
+        // 3 Call - recomended way
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+
+        require(callSuccess, "Call failed");
     }
 }
